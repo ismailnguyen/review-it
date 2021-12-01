e66128ae076954df03fce6ce53b69c0344cefaf1
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-12-01 18:07:54 +0100

    add issues link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,8 @@
 	<div class="container">
 		<h1>Review.it</h1>
 		<h3>Please contact us to support your store</h3>
+		<p>
+			Post an issue in our <a href="https://github.com/ismailnguyen/review-it/issues">Github repository</a> to contact us.
+		</p>
 	</div>
 </template>
